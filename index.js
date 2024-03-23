efd665a97e3c270042bb36695daef5d8737df36a
BilRos <billyrossowuor@gmail.com>
2024-03-23 12:29:56 +0300

    Added some arithmetic functions

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+   let num1=2;
+   let num2=31;
+   let multiply = num1 * num2 ;
+
+   const random = Math.ceil((Math.random() +1)*5)
+   Number.isInteger(random)
+
+   let num3=5;
+   let num4=19;
+   let mod = 19%5;
+
+   let max=(5,1,7,9,13,17,6,20);
+   Math.max(max);
